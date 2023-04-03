3f4c8d697223dbb129631084f01841b35f17d481
sunilsid6342 <sunilsid6342@gmail.com>
2023-04-03 22:56:05 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 var Tesseract = require("tesseract.js")
-// const tesseract = require("node-tesseract")not working in my laptop
-// const ReadText = require('text-from-image')not working in my laptop
-// var textract = require("textract") not working in my laptop
 const { createWorker } = require('tesseract.js');
 const fs = require('fs');
 const Axios = require('axios')
@@ -36,7 +33,7 @@ try {
     app.post("/url", async (req, res) => {
         const url = req.body.url
 
-        await downloadImage(url,'lena.png')
+        await downloadImage(url, 'lena.png')
 
 
         Tesseract.recognize(
@@ -45,9 +42,9 @@ try {
             { logger: m => console.log(m) }
         ).then(({ data: { text } }) => {
             const filteredText = Array.from(text.matchAll(/\d/g)).join("")
-            if(filteredText.length <=1){
+            if (filteredText.length <= 1) {
                 alpha()
-                async function alpha(){
+                async function alpha() {
                     const worker = await createWorker();//alphabet
                     (async () => {
                         await worker.loadLanguage('eng+chi_tra');
@@ -55,13 +52,13 @@ try {
                         const { data: { text } } = await worker.recognize('lena.png');
                         const tt = (text.trim()).split(" ").join("")
                         return res.send({
-                            output:tt
+                            output: tt
                         })
                     })();
                 }
-            }else{
+            } else {
                 return res.send({
-                    output:filteredText
+                    output: filteredText
                 })
             }
         })
@@ -79,47 +76,3 @@ app.listen(PORT, () => {
 })
 
 
-// res.send({
-//     input:text
-//   })
-
-
-// number ---------------------------
-// const { createWorker } = require('tesseract.js');
-
-// const worker = await createWorker();
-
-// (async () => {
-//   await worker.loadLanguage('eng');
-//   await worker.initialize('eng');
-//   await worker.setParameters({
-//     tessedit_char_whitelist: '0123456789',
-//   });
-//   const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-//   console.log(text);
-//   await worker.terminate();
-// })();
-
-// \n at last with alphabet ------
-// const worker = await createWorker();
-
-// (async () => {
-//   await worker.loadLanguage('eng+chi_tra');
-//   await worker.initialize('eng+chi_tra');
-//   const { data: { text } } = await worker.recognize('./lena.png');
-//   console.log(text);
-//   res.send({
-//     output : text
-//   })
-//   await worker.terminate();
-// })();
-
-//----- number for png  coorect
-// Tesseract.recognize(
-//     __dirname + '/lena.png',
-//     'eng',
-//     { logger: m => console.log(m) }
-// ).then(({ data: { text } }) => {
-//     const filteredText = Array.from(text.matchAll(/\d/g)).join("")
-//     console.log(filteredText)
-// })
